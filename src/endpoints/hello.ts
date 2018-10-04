afddb1671c84462e780bef8917eed57a622e38f0
Jaimin Darji <jaimin.d.88@gmail.com>
2018-10-03 21:46:13 -0400

    removed nit

--- src/endpoints/hello.ts
+++ src/endpoints/hello.ts
@@ -1,12 +1,9 @@
 import { Context, APIGatewayProxyEvent } from 'aws-lambda';
 
-import { nit } from '../nit_api';
-
 export default async function (event: APIGatewayProxyEvent, context: Context) {
-  const odds = nit(['.', '.'], 'As2c4d9s10c');
 
   return {
     statusCode: 200,
-    body: JSON.stringify(odds),
+    body: JSON.stringify({ hi: 1 }),
   };
 }
