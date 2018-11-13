486e3a0b925e86ae4982cd7e0e8a71583f8d40c8
Jaimin Darji <jaimin.d.88@gmail.com>
2018-11-13 11:15:54 -0800

    fixed test for random board

--- tests/nit_api/index.spec.ts
+++ tests/nit_api/index.spec.ts
@@ -11,11 +11,12 @@ describe('testing the pokerstove api in nodejs', async () => {
     expect(odds.hands[1].win).toBeCloseTo(0.49, 1);
   });
 
-  test('AsAc vs random hand', async () => {
+  test('AsAc vs random hand with a random board', async () => {
     const odds = await nit(['AsAc', '.'], '4s2c4d9hJs');
 
-    console.log(odds);
+    expect(odds.hands.length).toBe(2);
 
-    expect(1).toBe(2);
+    expect(odds.hands[0].win).toBeCloseTo(0.91, 1);
+    expect(odds.hands[1].win).toBeCloseTo(0.097, 1);
   });
 });
