61aaecfba0355f91bc035c7dd6ad457eb8e16475
Jaimin Darji <jaimin.d.88@gmail.com>
2018-12-13 17:49:30 -0500

    delete dist instead of build

--- src/endpoints/hello.ts
+++ src/endpoints/hello.ts
@@ -1,11 +1,6 @@
 import { Context, APIGatewayProxyEvent } from 'aws-lambda';
 
-import { nit } from '../nit_api';
-
 export async function handler(event: APIGatewayProxyEvent, context: Context) {
-
-  const odds = await nit(['.', '.'], 'As3s4c6c9d');
-
   return {
     statusCode: 200,
     body: JSON.stringify(event),
